621a4b3d6f23f6e050d0a8206ea2d6cdcbecf2c3
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-01-03 13:11:14 -0500

    use updated describe

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [duct/module.logging "0.4.0"]
                  [duct/module.web     "0.7.0"]
 
-                 [sweet-tooth/describe "0.1.0"]
+                 [sweet-tooth/describe "0.3.0"]
 
                  ;; server
                  [com.flyingmachine/liberator-unbound "0.2.0"]
