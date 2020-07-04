55d508f1ad5a372afbf90f13399b9e76333c9794
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-07-04 13:29:09 -0400

    better whitespace preservation of when inserting endpoint route

--- src/sweet_tooth/endpoint/generate.clj
+++ src/sweet_tooth/endpoint/generate.clj
@@ -1,6 +1,8 @@
 (ns sweet-tooth.endpoint.generate
   (:require [rewrite-clj.zip :as rz]
             [rewrite-clj.zip.whitespace :as rzw]
+            [rewrite-clj.node :as rn]
+            [rewrite-clj.custom-zipper.core :as rcz]
             [sweet-tooth.endpoint.system :as es]
             [clojure.string :as str]))
 
@@ -19,20 +21,21 @@
 
 (def endpoint-routes-point
   {:path ["cross" "endpoint_routes.cljc"]
-   :modify (fn [node ns-kw]
-             (-> node
-                 (rz/find-value rz/next 'serr/expand-routes)
-                 rz/right
-                 (rz/find-value rz/next 'comment)
-                 rz/up
-                 (rz/insert-right [ns-kw])
-                 rz/right
-                 rzw/insert-newline-left
-                 rzw/insert-space-left
-                 rzw/insert-space-left
-                 rzw/insert-space-left
-                 rzw/insert-space-left))})
+   :modify (fn [node form]
+             (let [comment-node (-> node
+                                    (rz/find-value rz/next 'serr/expand-routes)
+                                    rz/right
+                                    (rz/find-value rz/next 'comment)
+                                    rz/up)
+                   comment-left (rz/node (rcz/left comment-node))
+                   whitespace   (and (:whitespace comment-left) comment-left)]
+               (-> comment-node
+                   (rcz/insert-right form)
+                   rz/right
+                   rzw/insert-newline-left
+                   (rcz/insert-left whitespace))))})
 
-(defn update-file
+(defn update-file!
   [{:keys [modify] :as point} form]
-  (rz/root-string (modify (rz/of-file (path point)) form)))
+  (let [file-path (path point)]
+    (spit file-path (rz/root-string (modify (rz/of-file file-path) form)))))
